05d40ddcdaf488c2e32fce0721de62f81668a43d
Ayush Singh <ayushsingh1325@gmail.com>
2019-04-07 21:26:17 +0530

    feat: Added home_component.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,8 +1,10 @@
 import 'package:angular/angular.dart';
+import 'src/home_component.dart';
 
 @Component(
   selector: 'app-component',
   templateUrl: 'app_component.html',
+  directives: [HomeComponent],
 )
 class AppComponent {
   var name = 'Angular';
---
+++ lib/src/home_component.dart
@@ -0,0 +1,10 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'home-component',
+  templateUrl: 'home_component.html',
+  styleUrls: ['home_component.css'],
+)
+class HomeComponent {
+  var name = 'Ayush';
+}
